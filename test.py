9d792734ff77cd1b20078bd24bf886adca9401bf
lawtherLee <823583958@qq.com>
2026-01-21 00:59:03 +0800

    1

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
-# 统计字符串中，各个字符的个数，"hello world" 字符串统计的结果为： h:1 e:1 l:3 o:2 d:1 r:1
-def get_dict():
-    """
-    牛
-    :return:
-    """
-    str1 = "hello world"
-    str_dist = {}
-    for i in str1:
-        if i not in str_dist:
-            str_dist[i] = 1
+word_count = {}
+with open("input.txt", "r") as rf, open("output.txt", "w") as wf:
+    words = rf.read().split()
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
         else:
-            str_dist[i] += 1
-    print(str_dist)
+            word_count[word] = 1
+
+    for word, count in word_count.items():
+        wf.write(f"{word}:{count}\n")
