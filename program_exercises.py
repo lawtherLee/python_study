ba7f45432f085042fff294a0d5ffadd069f449cf
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2026-01-22 17:28:53 +0800

    测试题

--- program_exercises.py
+++ program_exercises.py
@@ -22,7 +22,7 @@ for i in range(1, n + 1):
 # 需求: 输出乘法表
 for i in range(1, 10):
     for j in range(1, i + 1):
-        print(f"{j} * {i} = {i * j}", end=' ')
+        print(f"{j} * {i} = {i * j}", end=" ")
     print(" ")
 
 # 基础的算法调优
@@ -39,9 +39,55 @@ start_time = time()
 for a in range(1001):
     for b in range(1001):
         c = 1000 - a - b
-        if a ** 2 + b ** 2 == c ** 2:  # 0.14秒
+        if a**2 + b**2 == c**2:  # 0.14秒
             print(f"a={a},b={b},c={c}")
 
 end_time = time()
 
 print(f"执行时长： {(end_time - start_time):.2f}秒")
+
+
+# 定义函数findall，要求返回符合要求的所有位置的起始下标，
+# 如字符串"helloworldhellopythonhelloc++hellojava"
+# 需要找出里面所有的"hello"的位置，返回的格式是一个元组，即：(0,10,21,29)
+def findall(s, target):
+    return tuple(
+        i for i in range(len(s) - len(target) + 1) if s[i : i + len(target)] == target
+    )
+
+
+result = findall("helloworldhellopythonhelloc++hellojava", "hello")
+print(result)
+
+
+def fun(*args):
+    """
+    计算传入参数的总和
+    参数可以是列表或字典的任意组合
+    如果是列表，则计算列表元素的总和
+    如果是字典，则计算字典值的总和
+    """
+    total = 0
+    for i in args:
+        if isinstance(i, list):
+            total += sum(i)
+        else:
+            total += sum(i.values())
+    return total
+
+
+str1 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def get_qrcode(str1):
+    """
+    获取4位数验证码
+    :param str1:
+    :return:
+    """
+    verify_code = random.choices(str1, k=4)
+    qrcode = "".join(verify_code)
+    return qrcode
+
+
+print(get_qrcode(str1))
--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-import ollama
+def find_words_by_group(words):
+    row1, row2, row3 = set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm")
+    res = []
+    for word in words:
+        lower_word = word.lower()
+        first_c = lower_word[0]
 
-print("Ollama Python 库版本：", ollama.__version__)
+        if first_c in row1:
+            target = row1
+        elif first_c in row2:
+            target = row2
+        else:
+            target = row3
+
+        is_all_in = all(c in target for c in lower_word)
+
+        if is_all_in:
+            res.append(word)
+    return res
+
+
+print(find_words_by_group(["Hello", "Alaska", "Dad", "Peace"]))
