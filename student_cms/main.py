c2da372bc5e23027a14355996a7fc338f5d78d90
lawtherLee <823583958@qq.com>
2026-01-26 23:05:47 +0800

    学生管理系统

--- student_cms/main.py
+++ student_cms/main.py
@@ -1,1 +1,6 @@
 # 面向对象编程
+from student_cms import *
+
+if __name__ == "__main__":
+    stu = StudentCms()
+    stu.start()
--- student_cms/student_cms.py
+++ student_cms/student_cms.py
@@ -1,3 +1,7 @@
+import time
+from student import Student
+
+
 class StudentCms(object):
     def __init__(self):
         self.student_list = []
@@ -15,10 +19,27 @@ class StudentCms(object):
         print("*" * 30)
 
     def add_student(self):
-        pass
+        name = input("请输入学生姓名：")
+        age = int(input("请输入学生年龄："))
+        gender = input("请输入学生性别：")
+        mobile = input("请输入学生手机号：")
+        desc = input("请输入学生描述：")
+        stu = Student(name, gender, age, mobile, desc)
+        self.student_list.append(stu)
+        print("添加学生信息成功")
 
     def update_student(self):
-        pass
+        update_name = input("请输入要修改的姓名：")
+        for stu in self.student_list:
+            if stu.name == update_name:
+                stu.name = input("请输入学生姓名：")
+                stu.age = int(input("请输入学生年龄："))
+                stu.gender = input("请输入学生性别：")
+                stu.mobile = input("请输入学生手机号：")
+                stu.desc = input("请输入学生描述：")
+                print("修改成功")
+                return
+        print("没有该学生")
 
     def delete_student(self):
         pass
@@ -27,7 +48,11 @@ class StudentCms(object):
         pass
 
     def show_all_student(self):
-        pass
+        if len(self.student_list) == 0:
+            print("暂无数据")
+        for stu in self.student_list:
+            print(stu)
+        print()
 
     def save_student(self):
         pass
@@ -36,8 +61,31 @@ class StudentCms(object):
         pass
 
     def start(self):
-        pass
+        self.load_student()
+        while True:
+            time.sleep(1)
+            self.show_view()
+
+            choice = input("请输入您的选择：")
+            if choice == "1":
+                self.add_student()
+            elif choice == "2":
+                self.update_student()
+            elif choice == "3":
+                self.delete_student()
+            elif choice == "4":
+                self.search_student()
+            elif choice == "5":
+                self.show_all_student()
+            elif choice == "0":
+                res = input("确定要退出系统吗？y/n")
+                if res.lower() == "y":
+                    print("欢迎下次再来")
+                    break
+            else:
+                print("输入错误，请重新输入")
 
 
 if __name__ == "__main__":
-    pass
+    stu1 = StudentCms()
+    stu1.start()
